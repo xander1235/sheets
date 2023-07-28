c3ecb1aa658079598f1a5a18bf55d35ebd252934
xander1235 <rathodveerender25@gmail.com>
2023-07-29 00:59:27 +0530

    taking credentails from environment

--- src/main/java/com/triffy/sheet/config/GoogleAuthorizationConfig.java
+++ src/main/java/com/triffy/sheet/config/GoogleAuthorizationConfig.java
@@ -2,15 +2,15 @@ package com.triffy.sheet.config;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
@@ -18,12 +18,13 @@ import java.util.Collections;
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class GoogleAuthorizationConfig {
 
     @Value("${application.name}")
     private String applicationName;
-    @Value("${credentials.file.path}")
-    private String credentialsFilePath;
+    @Value("${credentials.content}")
+    private String credentialsContent;
 
 
     @Bean
@@ -32,8 +33,7 @@ public class GoogleAuthorizationConfig {
 
         // Initializing the service:
         GoogleCredential googleCredentials;
-        try(InputStream inputSteam = getClass().getResourceAsStream(credentialsFilePath)) {
-            assert inputSteam != null;
+        try(InputStream inputSteam = new ByteArrayInputStream(credentialsContent.getBytes())) {
             googleCredentials = GoogleCredential.fromStream(inputSteam).createScoped(SCOPES);
         }
 
